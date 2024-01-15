a207d02787800e641c103b77c82f31ce0bf44ed1
OXAYR <130404981+OXAYR@users.noreply.github.com>
2024-01-15 12:52:42 +0500

    added settings page for my block

--- test-block.php
+++ test-block.php
@@ -28,3 +28,122 @@ function test_block_test_block_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'test_block_test_block_block_init' );
+
+
+/**
+ * Test Block settings page
+ */
+
+
+
+function test_block_register_settings() {
+    add_option('test_block_text', 'Hello, default text!');
+    add_option('test_block_content_color', '#000000');
+    add_option('test_block_padding', array(
+        'top'    => '10px',
+        'right'  => '10px',
+        'bottom' => '10px',
+        'left'   => '10px',
+    ));
+}
+
+function test_block_settings_menu() {
+    add_menu_page(
+		//Page Title
+        'Test Block Settings',
+		//Settings Title 
+        'Test Block',
+		// Allowing the user to manage options
+        'manage_options',
+
+        'test_block_settings',
+        // function which would render on the load of the settings page 
+		'test_block_settings_page'
+    );
+}
+
+function test_block_settings_page() {
+    ?>
+    <div class="wrap">
+        <h2>Test Block Settings</h2>
+        <form method="post" action="options.php">
+            <?php
+            settings_fields('test_block_options');
+            do_settings_sections('test_block_settings');
+            submit_button();
+            ?>
+        </form>
+    </div>
+    <?php
+}
+
+function test_block_init_settings() {
+    register_setting('test_block_options', 'test_block_text');
+    register_setting('test_block_options', 'test_block_content_color');
+    register_setting('test_block_options', 'test_block_padding');
+}
+
+function test_block_settings_fields() {
+    add_settings_section(
+        'test_block_main_section',
+        'Main Settings',
+        'test_block_main_section_cb',
+        'test_block_settings'
+    );
+
+    add_settings_field(
+        'test_block_text',
+        'Default Text',
+        'test_block_text_cb',
+        'test_block_settings',
+        'test_block_main_section'
+    );
+
+    add_settings_field(
+        'test_block_content_color',
+        'Content Color',
+        'test_block_content_color_cb',
+        'test_block_settings',
+        'test_block_main_section'
+    );
+
+    add_settings_field(
+        'test_block_padding',
+        'Padding',
+        'test_block_padding_cb',
+        'test_block_settings',
+        'test_block_main_section'
+    );
+}
+
+function test_block_main_section_cb() {
+    echo '<p>Main settings for Test Block.</p>';
+}
+
+function test_block_text_cb() {
+    $text = get_option('test_block_text', 'Hello, default text!');
+    echo "<input type='text' name='test_block_text' value='{$text}' />";
+}
+
+function test_block_content_color_cb() {
+    $content_color = get_option('test_block_content_color', '#000000');
+    echo "<input type='text' name='test_block_content_color' value='{$content_color}' class='color-field' />";
+}
+
+function test_block_padding_cb() {
+    $padding = get_option('test_block_padding', array(
+        'top'    => '10px',
+        'right'  => '10px',
+        'bottom' => '10px',
+        'left'   => '10px',
+    ));
+
+    foreach ($padding as $key => $value) {
+        echo "<label>{$key}</label> <input type='text' name='test_block_padding[{$key}]' value='{$value}' /><br />";
+    }
+}
+
+add_action('admin_init', 'test_block_init_settings');
+add_action('admin_menu', 'test_block_settings_menu');
+add_action('admin_init', 'test_block_register_settings');
+add_action('admin_init', 'test_block_settings_fields');
